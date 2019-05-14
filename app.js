2099cfd8cb4257f5013f3a5b5811c1d7d4b41892
Miles Low <contact@momentapp.us>
2019-05-14 07:04:48 -0700

    yaya

--- app.js
+++ app.js
@@ -5,8 +5,33 @@ const helmet = require("helmet");
 const bcrypt = require("bcrypt");
 const app = express();
 
+function passwordFromID(id) {
+  return 'SELECT `password` from `user` WHERE (`user_id`) = ("' + id + '")';
+}
+
 function existsQuery(email) {
-  return 'SELECT `password` from `user` WHERE (`email`) = ("' + email + '")';
+  return (
+    'SELECT `password`, `user_id` from `user` WHERE (`email`) = ("' +
+    email +
+    '")'
+  );
+}
+function userQuery(id) {
+  return (
+    'SELECT `full_name`, `email` from `user` WHERE (`user_id`) = ("' + id + '")'
+  );
+}
+
+function editQuery(email, name, id) {
+  return (
+    'UPDATE `user` SET `email`= "' +
+    email +
+    '", `full_name`= "' +
+    name +
+    '" WHERE `user_id` = "' +
+    id +
+    '"'
+  );
 }
 
 function makeQuery(body) {
@@ -25,9 +50,9 @@ function makeQuery(body) {
 }
 
 const config = {
-  host: "localhost",
+  host: "35.236.107.146",
   user: "root",
-  password: "123",
+  password: "av7ijiev#8A4u%l0BGXB",
   database: "glutenMaps"
 };
 const saltRounds = 5;
@@ -61,25 +86,58 @@ con.connect(function(error) {
 
   app.post("/login", (req, res) => {
     const query = existsQuery(req.body.email);
-    var responseBody = "true";
+    var responseBody = { verified: true };
     var statusCode = 200;
     con.query(query, function(error, result) {
       if (error) throw error;
       if (result[0] === undefined) {
-        responseBody = false;
+        responseBody.verified = false;
         statusCode = 422;
         res.status(statusCode).send(responseBody);
       } else {
         const hashedPassword = result[0].password;
         const password = req.body.password;
+        const id = result[0].user_id;
         bcrypt.compare(password, hashedPassword, function(err, result) {
           if (!result) {
             statusCode = 422;
-            responseBody = false;
+            responseBody.verified = false;
           }
+          responseBody.id = id;
           res.status(statusCode).send(responseBody);
         });
       }
+    });
+  });
+  app.post("/userData", (req, res) => {
+    const query = userQuery(req.body.id);
+    con.query(query, function(error, result) {
+      if (error) throw error;
+      res.status(200).send(result[0]);
+    });
+  });
+  app.post("/editUser", (req, res) => {
+    const query = passwordFromID(req.body.id);
+    con.query(query, function(error, result) {
+      if (error) throw error;
+      const password = req.body.password;
+      const hashedPassword = result[0].password;
+      bcrypt.compare(password, hashedPassword, function(err, result) {
+        if (err) throw err;
+        if (result) {
+          const query2 = editQuery(
+            req.body.email,
+            req.body.full_name,
+            req.body.id
+          );
+          con.query(query2, function(error2, result2) {
+            if (error2) throw error2;
+            res.status(200).send(true);
+          });
+        } else {
+          res.status(422).send(false);
+        }
+      });
     });
   });
 });
