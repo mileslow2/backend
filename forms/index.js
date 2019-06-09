826862fde10840c2334ade805aeedde677b67b0c
Miles Low <contact@momentapp.us>
2019-06-09 11:18:09 -0700

    Fixed a file path issue

--- forms/index.js
+++ forms/index.js
@@ -1,7 +1,7 @@
-const editUser = require("./forms/editUser");
-const register = require("./forms/register");
-const login = require("./forms/login");
-const getUserInfo = require("./forms/getUserInfo");
+const editUser = require("./editUser");
+const register = require("./register");
+const login = require("./login");
+const getUserInfo = require("./getUserInfo");
 
 module.exports = (app, con) => {
   register(app, con);
