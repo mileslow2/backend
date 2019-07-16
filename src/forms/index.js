50a52f0612658426c30aa303048cff856acc401d
Miles Low <contact@momentapp.us>
2019-07-16 13:12:02 -0400

    forgot about adding this in the last commit

--- src/forms/index.js
+++ src/forms/index.js
@@ -2,10 +2,14 @@ const editUser = require("./editUser");
 const register = require("./register");
 const login = require("./login");
 const getUserInfo = require("./getUserInfo");
-
-module.exports = app => {
-  register(app);
-  login(app);
-  getUserInfo(app);
-  editUser(app);
+const getPlaces = require('./getPlaces');
+const getPlaceInfo = require('./getPlaceInfo');
+module.exports = app =>
+{
+    getPlaceInfo(app);
+    register(app);
+    login(app);
+    getUserInfo(app);
+    editUser(app);
+    getPlaces(app);
 };
