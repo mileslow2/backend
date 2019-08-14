97064ef07631941447d76fb627c0cdb74e9529a1
Miles Low <contact@momentapp.us>
2019-08-14 15:26:03 -0700

    feedback email works

--- test/index.js
+++ test/index.js
@@ -1,8 +1,9 @@
 require('dotenv').config();
-require('./user');
-require('./loginSignup');
-require('./review');
+// require('./user');
+// require('./loginSignup');
+// require('./review');
 // require("./place");
 
 // this is for email testing
+require('./feedback/feedback');
 // require('./JWT/jwt');
